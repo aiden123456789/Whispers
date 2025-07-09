c9708db56ffff2a4cd9afa50fca4b09f7f1165ea
Aiden <aidenbenise@gmail.com>
2025-07-08 23:06:33 -0400

    compartmentalization stable pt 3

--- src/app/components/Events.tsx
+++ src/app/components/Events.tsx
@@ -6,7 +6,7 @@ import dynamic from 'next/dynamic';
  * Dynamically import the Leaflet‑based map **only on the client** to avoid
  * “window is not defined” errors during SSR.
  */
-const EventsMap = dynamic(() => import('./EventsMap'), { ssr: false });
+const EventsMap = dynamic(() => import('./EventsMap/EventsMap'), { ssr: false });
 
 export default function Events() {
   return <EventsMap />;
