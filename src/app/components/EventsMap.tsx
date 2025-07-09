4e9b67c578867402fc8dd129f6aa4f25acfb665d
Aiden <aidenbenise@gmail.com>
2025-07-08 22:08:53 -0400

    greendots plz pt 2

--- src/app/components/EventsMap.tsx
+++ src/app/components/EventsMap.tsx
@@ -2,7 +2,6 @@
 
 import { useRef, useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
-import 'leaflet/dist/leaflet.css';
 import { DivIcon } from 'leaflet';
 
 const MapContainer = dynamic(() => import('react-leaflet').then(m => m.MapContainer), { ssr: false });
