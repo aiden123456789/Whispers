3a86c33228366509b9c9d0192339a3a14331d432
Aiden <aidenbenise@gmail.com>
2025-07-11 23:13:08 -0400

    fixing rate limit pt 2

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.akingb15.whispers',
   appName: 'Whispers',
-  webDir: 'dist'
+  webDir: 'dist',
   server: {
     url: 'https://whispers-six.vercel.app', // 👈 your live website
     cleartext: true
-  }
+  },
 };
 
 export default config;
